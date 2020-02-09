a2355c97396a244f160ab9d6e7f66efdc2e0218d
Sungho Park <siminsungho@gmail.com>
2020-02-09 22:35:08 +0900

    Add config option "NRF5SDK__IOT_ENABLED".

--- config/nrf5sdk.cmake
+++ config/nrf5sdk.cmake
@@ -9,6 +9,7 @@ set_cache_default(NRF5SDK__SYSTICK_ENABLED                                      
 set_cache_default(NRF5SDK__UART_ENABLED                                         FALSE   BOOL "")
 set_cache_default(NRF5SDK__FREERTOS                                             FALSE   BOOL "Include freertos component")
 set_cache_default(NRF5SDK__CRYPTO_ENABLED                                       FALSE   BOOL "")
+set_cache_default(NRF5SDK__IOT_ENABLED                                          FALSE   BOOL "")
 
 set_cache_default(NRF5SDK__SWI_DISABLE0                                         FALSE   BOOL "Exclude SWI0 from being utilized by the driver")
 set_cache_default(NRF5SDK__APP_TIMER_V2                                         FALSE   BOOL "")
